3434f903a1c4379f13980480185c840025082c71
Norbert <norbert.mauger@gmail.com>
2023-01-23 14:14:48 +0100

    Added header

--- App.tsx
+++ App.tsx
@@ -1,49 +1,54 @@
 import { StatusBar } from "expo-status-bar";
 import { Fragment, useState } from "react";
-import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Button, Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 
 
 export type Props = {
   title: string;
-  children?: 
+  children?:
   | React.ReactNode
   | React.ReactNode[];
 };
 
+import * as React from "react";
+import { Header, Icon } from  '@rneui/themed';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world, how are you there?</Text>
-      <Text>Notification test</Text>
-      <MyComponent title="title">
-        <Text>*** Another Awesome Text here... ***</Text>
-        test
-      </MyComponent>
-      <StatusBar style="auto"/>
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <SafeAreaView style={styles.container}>
+        <Header 
+          backgroundImageStyle={{}}
+          barStyle="default"
+          centerComponent={{
+            text: "MY TITLE",
+            style: { color: "#fff" }
+          }}
+          centerContainerStyle={{}}
+          containerStyle={{ width: 350 }}
+          leftComponent={{ icon: "menu", color: "#fff" }}
+          leftContainerStyle={{}}
+          placement="center"
+          rightComponent={{ icon: "home", color: "#fff" }}
+          rightContainerStyle={{}}
+          statusBarProps={{}} />
+        <ScrollView>
+          <Cafe></Cafe>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 }
-
-const MyComponent: React.FC<Props> = ({ title, children }) => {
-  return (
-    <Fragment>
-      <Text>
-        {title}
-      </Text>
-      <Text>
-        {children}
-      </Text>
-      <Cafe></Cafe>
-      
-    </Fragment>
-  );
-};
 
 const Cafe = () => {
   return (
     <>
       <Cat name="Willy" />
       <Cat name="Spot" />
+      <Cat name="Tommy" />
+      <Cat name="Lilly" />
     </>
   );
 };
@@ -51,36 +56,34 @@ const Cafe = () => {
 type CatProps = {
   name: string;
 }
-const Cat: React.FC<CatProps> = ({name}) => {
+const Cat: React.FC<CatProps> = ({ name }) => {
   const [isHungry, setIsHungry] = useState(true);
   const [digestionTime, resetTime] = useState(Math.floor(Math.random() * 5000));
 
-  return(
+  return (
     <Fragment>
-      <ScrollView>
       <View>
         <Text>Meow, my name is {name}</Text>
-        <Text>and {isHungry?"I am hungry":"I'm fine thank you"}</Text>
+        <Text>and {isHungry ? "I am hungry" : "I'm fine thank you"}</Text>
         <Image
           source={{
             uri: 'https://reactnative.dev/docs/assets/p_cat1.png',
           }}
-          style={{width: 200, height: 200}}
-        />  
+          style={{ width: 200, height: 200 }}
+        />
         <Button
-        onPress={() => {
-          setIsHungry(false);
-          let timer: ReturnType<typeof setTimeout> = setTimeout(() => { 
-            setIsHungry(true);
-            clearTimeout(timer);
-            resetTime(Math.floor(Math.random() * 5000))
-          }, digestionTime);
-        }}
-        disabled={!isHungry}
-        title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
-      />
+          onPress={() => {
+            setIsHungry(false);
+            let timer: ReturnType<typeof setTimeout> = setTimeout(() => {
+              setIsHungry(true);
+              clearTimeout(timer);
+              resetTime(Math.floor(Math.random() * 5000))
+            }, digestionTime);
+          }}
+          disabled={!isHungry}
+          title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
+        />
       </View>
-      </ScrollView>
     </Fragment>
   )
 }
